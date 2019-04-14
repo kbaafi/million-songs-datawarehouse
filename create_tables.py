3e7af85cf18b7ad3adefe0a908440a7b4f7ab301
Kofi Baafi <kbaafi@gmail.com>
2019-04-15 00:03:38 +0300

    connections update

--- create_tables.py
+++ create_tables.py
@@ -10,7 +10,7 @@ from sql_queries import create_table_queries, drop_table_queries
 """
 def create_database():
     # connect to default database
-    conn = psycopg2.connect("host=127.0.0.1 dbname=studentdb user=student password=student")
+    conn = # set up your data connection
     conn.set_session(autocommit=True)
     cur = conn.cursor()
     
